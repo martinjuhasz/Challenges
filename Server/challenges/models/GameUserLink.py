aa1bcb0546696028edec3d42f2e30629228a3f9c
Martin Juhasz <info@martinjuhasz.de>
2015-06-03 21:23:04 +0200

    added many to many relationship

--- Server/challenges/models/GameUserLink.py
+++
@@ -1,9 +0,0 @@
-from challenges.app import db
-from sqlalchemy import Column, Integer, String, ForeignKey
-
-class GameUserLink(db.Model):
-    __tablename__ = 'game_user_link'
-
-    id = Column(Integer, primary_key=True)
-    game_id = Column(Integer, ForeignKey('games.id'), primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
